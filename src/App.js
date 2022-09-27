e4715dd659f4a256206f7f0d16eb517551065101
runtee <ekwo.chinonso.01@gmail.com>
2022-09-27 17:57:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 
+import UserAvater from "./components/userAvater";
 function App() {
-  return <div className="bg-[#F5F7FB] w-screen h-screen">Tex Messenger</div>;
+  return <div className="bg-[#F5F7FB] w-screen h-screen">Tex Messenger
+  <UserAvater/>
+  </div>;
 }
 
 export default App;
