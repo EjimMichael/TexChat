9e324c05fb92fdb0c82e0d4cded55c64cf9026d4
runtee <ekwo.chinonso.01@gmail.com>
2022-09-28 17:24:44 +0100

    added some coponents

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 
+import Contact from "./components/contact";
 import UserAvater from "./components/userAvater";
 function App() {
   return <div className="bg-[#F5F7FB] w-screen h-screen">Tex Messenger
-  <UserAvater/>
+  {/* <UserAvater/> */}
+  
+  <div class="flex flex-row">
+  <div class="basis-1/3 p-5" ><Contact/></div>
+  <div class="basis-2/3">03</div>
+  
+</div>
+
   </div>;
 }
 
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,3 +1,39 @@
-export default function Contact() {
+export default function Contact() {  
+        const people = [
+            {
+              name: 'Calvin Hawkins',
+              email: 'calvin.hawkins@example.com',
+              image:
+                'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            },
+            {
+              name: 'Kristen Ramos',
+              email: 'kristen.ramos@example.com',
+              image:
+                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            },
+            {
+              name: 'Ted Fox',
+              email: 'ted.fox@example.com',
+              image:
+                'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            },
+          ]
+          
+            return (
+              <ul className="divide-y divide-gray-200">
+                {people.map((person) => (
+                  <li key={person.email} className="py-4 flex">
+                    <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
+                    <div className="ml-3 mr-auto">
+                      <p className="text-sm font-medium text-gray-900">{person.name}</p>
+                      <p className="text-sm text-gray-500">{person.email}</p>
+                    </div>
+                    <span class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-red-100 bg-green-600 rounded-full">9</span>
+                    
+                  </li>
+                ))}
+              </ul>
+            )
     
-}
+                }    
